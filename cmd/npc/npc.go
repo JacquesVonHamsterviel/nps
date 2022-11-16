d7d26b489686cb257ed543a756094c70e53eeb0f
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2022-11-16 20:43:11 +0000

    NPC_P2P_Only

--- cmd/npc/npc.go
+++ cmd/npc/npc.go
@@ -5,7 +5,7 @@ import (
 	"ehang.io/nps/lib/common"
 	"ehang.io/nps/lib/config"
 	"ehang.io/nps/lib/file"
-	"ehang.io/nps/lib/install"
+	//"ehang.io/nps/lib/install"
 	"ehang.io/nps/lib/version"
 	"flag"
 	"fmt"
@@ -17,7 +17,8 @@ import (
 	"runtime"
 	"strings"
 	"sync"
-	"time"
+	"strconv"
+	//"time"
 )
 
 var (
@@ -27,7 +28,7 @@ var (
 	logType        = flag.String("log", "stdout", "Log output mode（stdout|file）")
 	connType       = flag.String("type", "tcp", "Connection type with the server（kcp|tcp）")
 	proxyUrl       = flag.String("proxy", "", "proxy socks5 url(eg:socks5://111:222@127.0.0.1:9007)")
-	logLevel       = flag.String("log_level", "7", "log level 0~7")
+	logLevel       = flag.String("log_level", "1", "log level 0~7")
 	registerTime   = flag.Int("time", 2, "register time long /h")
 	localPort      = flag.Int("local_port", 2000, "p2p local port")
 	password       = flag.String("password", "", "p2p password flag")
@@ -65,27 +66,40 @@ func main() {
 	// init service
 	options := make(service.KeyValue)
 	svcConfig := &service.Config{
-		Name:        "Npc",
-		DisplayName: "nps内网穿透客户端",
-		Description: "一款轻量级、功能强大的内网穿透代理服务器。支持tcp、udp流量转发，支持内网http代理、内网socks5代理，同时支持snappy压缩、站点保护、加密传输、多路复用、header修改等。支持web图形化管理，集成多用户模式。",
+		Name:        "NPC",
+		DisplayName: "NPC",
+		Description: "NPC",
 		Option:      options,
 	}
-	if !common.IsWindows() {
-		svcConfig.Dependencies = []string{
-			"Requires=network.target",
-			"After=network-online.target syslog.target"}
-		svcConfig.Option["SystemdScript"] = install.SystemdScript
-		svcConfig.Option["SysvScript"] = install.SysvScript
+	//if !common.IsWindows() {
+	//	svcConfig.Dependencies = []string{
+	//		"Requires=network.target",
+	//		"After=network-online.target syslog.target"}
+	//	svcConfig.Option["SystemdScript"] = install.SystemdScript
+	//	svcConfig.Option["SysvScript"] = install.SysvScript
+	//}
+	
+
+	if len(os.Args) >= 2 {
+		switch os.Args[1] {
+		case "nat":
+			c := stun.NewClient()
+			c.SetServerAddr(*stunAddr)
+			nat, host, err := c.Discover()
+			if err != nil || host == nil {
+				logs.Error("get nat type error", err)
+				fmt.Printf("get nat type error", err)
+				return
+			}
+			fmt.Printf("nat type: %s \npublic address: %s\n", nat.String(), host.String())
+			os.Exit(0)
+
+		default:
+			fmt.Printf("无效的参数\n")
+			fmt.Printf("你可以使用携带nat参数运行以检查你的NAT类型\n")
 	}
-	for _, v := range os.Args[1:] {
-		switch v {
-		case "install", "start", "stop", "uninstall", "restart":
-			continue
-		}
-		if !strings.Contains(v, "-service=") && !strings.Contains(v, "-debug=") {
-			svcConfig.Arguments = append(svcConfig.Arguments, v)
-		}
-	}
+}
+
 	svcConfig.Arguments = append(svcConfig.Arguments, "-debug=false")
 	prg := &npc{
 		exit: make(chan struct{}),
@@ -100,73 +114,7 @@ func main() {
 		wg.Wait()
 		return
 	}
-	if len(os.Args) >= 2 {
-		switch os.Args[1] {
-		case "status":
-			if len(os.Args) > 2 {
-				path := strings.Replace(os.Args[2], "-config=", "", -1)
-				client.GetTaskStatus(path)
-			}
-		case "register":
-			flag.CommandLine.Parse(os.Args[2:])
-			client.RegisterLocalIp(*serverAddr, *verifyKey, *connType, *proxyUrl, *registerTime)
-		case "update":
-			install.UpdateNpc()
-			return
-		case "nat":
-			c := stun.NewClient()
-			c.SetServerAddr(*stunAddr)
-			nat, host, err := c.Discover()
-			if err != nil || host == nil {
-				logs.Error("get nat type error", err)
-				return
-			}
-			fmt.Printf("nat type: %s \npublic address: %s\n", nat.String(), host.String())
-			os.Exit(0)
-		case "start", "stop", "restart":
-			// support busyBox and sysV, for openWrt
-			if service.Platform() == "unix-systemv" {
-				logs.Info("unix-systemv service")
-				cmd := exec.Command("/etc/init.d/"+svcConfig.Name, os.Args[1])
-				err := cmd.Run()
-				if err != nil {
-					logs.Error(err)
-				}
-				return
-			}
-			err := service.Control(s, os.Args[1])
-			if err != nil {
-				logs.Error("Valid actions: %q\n%s", service.ControlAction, err.Error())
-			}
-			return
-		case "install":
-			service.Control(s, "stop")
-			service.Control(s, "uninstall")
-			install.InstallNpc()
-			err := service.Control(s, os.Args[1])
-			if err != nil {
-				logs.Error("Valid actions: %q\n%s", service.ControlAction, err.Error())
-			}
-			if service.Platform() == "unix-systemv" {
-				logs.Info("unix-systemv service")
-				confPath := "/etc/init.d/" + svcConfig.Name
-				os.Symlink(confPath, "/etc/rc.d/S90"+svcConfig.Name)
-				os.Symlink(confPath, "/etc/rc.d/K02"+svcConfig.Name)
-			}
-			return
-		case "uninstall":
-			err := service.Control(s, os.Args[1])
-			if err != nil {
-				logs.Error("Valid actions: %q\n%s", service.ControlAction, err.Error())
-			}
-			if service.Platform() == "unix-systemv" {
-				logs.Info("unix-systemv service")
-				os.Remove("/etc/rc.d/S90" + svcConfig.Name)
-				os.Remove("/etc/rc.d/K02" + svcConfig.Name)
-			}
-			return
-		}
-	}
+	
 	s.Run()
 }
 
@@ -205,42 +153,20 @@ func (p *npc) run() error {
 
 func run() {
 	common.InitPProfFromArg(*pprofAddr)
-	//p2p or secret command
-	if *password != "" {
-		commonConfig := new(config.CommonConfig)
-		commonConfig.Server = *serverAddr
-		commonConfig.VKey = *verifyKey
-		commonConfig.Tp = *connType
-		localServer := new(config.LocalServer)
-		localServer.Type = *localType
-		localServer.Password = *password
-		localServer.Target = *target
-		localServer.Port = *localPort
-		commonConfig.Client = new(file.Client)
-		commonConfig.Client.Cnf = new(file.Config)
-		go client.StartLocalServer(localServer, commonConfig)
-		return
-	}
-	env := common.GetEnvMap()
-	if *serverAddr == "" {
-		*serverAddr, _ = env["NPC_SERVER_ADDR"]
-	}
-	if *verifyKey == "" {
-		*verifyKey, _ = env["NPC_SERVER_VKEY"]
-	}
+	commonConfig := new(config.CommonConfig)
+	commonConfig.Server = "1.1.1.1:8088"
+	commonConfig.VKey = "YourvVkey"
+	commonConfig.Tp = "tcp"
+	localServer := new(config.LocalServer)
+	localServer.Type = "p2p"
+	localServer.Password = "YourPassword"
+	localServer.Target = "10.0.0.2:30000" 
+	localServer.Port = 3000 
+	commonConfig.Client = new(file.Client)
+	commonConfig.Client.Cnf = new(file.Config)
+	go client.StartLocalServer(localServer, commonConfig)
+	fmt.Printf("启动成功！！！\n")
+	exec.Command(`cmd`, `/c`, `start`, `http://127.0.0.1:`+strconv.Itoa(localServer.Port)).Start()
+	//return
 	logs.Info("the version of client is %s, the core version of client is %s", version.VERSION, version.GetVersion())
-	if *verifyKey != "" && *serverAddr != "" && *configPath == "" {
-		go func() {
-			for {
-				client.NewRPClient(*serverAddr, *verifyKey, *connType, *proxyUrl, nil, *disconnectTime).Start()
-				logs.Info("Client closed! It will be reconnected in five seconds")
-				time.Sleep(time.Second * 5)
-			}
-		}()
-	} else {
-		if *configPath == "" {
-			*configPath = common.GetConfigPath()
-		}
-		go client.StartFromFile(*configPath)
-	}
 }
